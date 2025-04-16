f777ea6d79dae34bbf25bc7c57566d4b77c1cffc
Juan L C <202941136+jlcarg@users.noreply.github.com>
2025-04-16 19:55:31 +0900

    Write logic to get computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log("hello")
+// Create computer's random choice function
+function getComputerChoice () {
+
+    let randomInt = getRandomInt(1, 4);
+    let computerChoice;
+    switch (randomInt) {
+        case 1:
+            computerChoice = "Rock";
+            break;
+        case 2:
+            computerChoice = "Paper";
+            break;
+        case 3:
+            computerChoice = "Scissors";
+            break;
+    }
+    return computerChoice
+
+}
+
+function getRandomInt (min, max) {
+
+    let randomNumber = Math.floor(Math.random() * (max - min) + min);
+    return randomNumber
+
+}
+
+console.log(`Computer's choice is: ${getComputerChoice()}`)
+
+
+// Create user's choice function with input
+
+// Create Function that compares both choices and returns Winner
+
+// Create logic to keep track of score
+
+// Create logic to play 5 rounds
