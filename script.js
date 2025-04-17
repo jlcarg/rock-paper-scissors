38fc702efdf94caf94bacd1ae05932e6b11cd1b2
Juan L C <202941136+jlcarg@users.noreply.github.com>
2025-04-17 16:33:08 +0900

    Write logic for playing a round of the game

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let humanScore = 0;
 let computerScore = 0;
+let roundNumber = 1;
 
 // Create computer's random choice function
 function getComputerChoice () {
@@ -31,18 +32,47 @@ function getRandomInt (min, max) {
 // Create user's choice function with input
 
 function  getHumanChoice () {
-    let humanChoice =  prompt("Please select between Rock, Paper and Scissors:\n");
+
+    let humanChoice =  prompt("Please select between Rock, Paper and Scissors:");
     humanChoice = humanChoice[0].toUpperCase() + humanChoice.substring(1).toLowerCase();
     return humanChoice
+
 }
 
-console.log(`Human's choice is: ${getHumanChoice()}`)
-console.log(`Computer's choice is: ${getComputerChoice()}`)
+
 
 // Write logic to play single round
 
+function playRound (humanChoice, computerChoice) {
 
+    console.log(`Round number ${roundNumber}`)
+    console.log(`Human's choice is: ${humanChoice}`)
+    console.log(`Computer's choice is: ${computerChoice}`)
 
+    if (humanChoice === computerChoice) {
 
+        console.log(`It's a draw! You both chose ${humanChoice}!`);
+
+    } else if (humanChoice === "Rock" && computerChoice === "Scissors" ||
+        humanChoice === "Paper"  && computerChoice === "Rock" ||
+        humanChoice === "Scissors" && computerChoice === "Paper") {
+
+        console.log(`You Win! ${humanChoice} beats ${computerChoice}!`);
+        humanScore += 1;
+
+    } else {
+
+        console.log(`You Lose! ${computerChoice} beats ${humanChoice}!`);
+        computerScore += 1;
+    }
+
+    console.log(`The current score is: \nPlayer ${humanScore} x ${computerScore} Computer`);
+    roundNumber += 1;
+
+    return
+
+}
+
+playRound(getHumanChoice(), getComputerChoice());
 
 // Create logic to play 5 rounds
